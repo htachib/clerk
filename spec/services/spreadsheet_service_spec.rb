9f915e150a26f5b93bd7b76e877a1da82e816b4e
Ryan Kulp <ryanckulp@gmail.com>
2019-02-28 02:35:03 +0800

    begin testing services

---
+++ spec/services/spreadsheet_service_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe SpreadsheetService do
+  before do
+    allow_any_instance_of(Google::Auth::UserRefreshCredentials).to receive(:fetch_access_token!).and_return({})
+
+    stub_request(:get, "https://api.docparser.com/v1/results/eylfucfqzted?api_key=ea69446c9b43ec90559a3f271ec0eb0e06e711a0").
+      to_return(status: 200, body: "", headers: {})
+  end
+
+  subject { described_class.new(create(:user)) }
+
+  describe '#import_data' do
+    it 'should import documents and upload data to Google Sheets' do
+      expect {
+        subject.import_data
+      }.to change { Parser.count }.by(3)
+    end
+
+    it 'should return user' do
+      expect(subject.user).to eql User.last
+    end
+  end
+end

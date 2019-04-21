708d875f4aae494a42742c98d9352356891b5608
Hideko <hideko.tachibana@gmail.com>
2019-04-21 23:58:11 +0700

    scope for already processed docs

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,6 +1,8 @@
 class Document < ActiveRecord::Base
   belongs_to :parser
 
+  scope :processed, -> { where(processed: true) }
+
   def process!
     update(processed: true)
     # TODO: move file from G Drive to '{{ parser_name }} - BACKUP' folder

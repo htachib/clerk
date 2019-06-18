55e6d921eb6b2f8a485c3b2f2b5d041c85fd666b
Hideko <hideko.tachibana@gmail.com>
2019-06-18 15:14:53 +0200

    add db seeds for new kehe templates, start kehe chargeback invoice homestore billing parser

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,20 @@ admin = User.find_by(admin_params)
 User.create!(admin_params.merge('password' => 'password', 'password_confirmation' => 'password')) if admin.nil?
 
 # prepare: Parser.all.map {|p| p.attributes.except('created_at', 'id', 'updated_at', 'user_id')}
-parsers = [{"external_id"=>"1Z5by0rrvu1tVw5K2nXZRAJL3ct2TM7GS", "name"=>"UNFI East Weekly MCB", "destination_id"=>"1pWmdUxPRO7Id3HkhlsDJfTViGLKzdfIVKZ_Ad0memkE", "settings"=>{"source"=>"google_drive", "library"=>"UNFIEastWeeklyMCB"}}, {"external_id"=>"xvexmuksclhe", "name"=>nil, "destination_id"=>"1pWmdUxPRO7Id3HkhlsDJfTViGLKzdfIVKZ_Ad0memkE", "settings"=>{"library"=>"UNFIWestWeeklyMCB"}}, {"external_id"=>"eylfucfqzted", "name"=>nil, "destination_id"=>"1pWmdUxPRO7Id3HkhlsDJfTViGLKzdfIVKZ_Ad0memkE", "settings"=>{"library"=>"UNFIEastChargeback"}}, {"external_id"=>"enowqxdfgcqg", "name"=>nil, "destination_id"=>"1pWmdUxPRO7Id3HkhlsDJfTViGLKzdfIVKZ_Ad0memkE", "settings"=>{"library"=>"UNFIEastDeductionQuarterly"}}, {"external_id"=>"azwkpkgfxroi", "name"=>"UNFI East Reclamation", "destination_id"=>"1pWmdUxPRO7Id3HkhlsDJfTViGLKzdfIVKZ_Ad0memkE", "settings"=>{"library"=>"UNFIEastReclamation"}}, {"external_id"=>"unkxjvdpcdwg", "name"=>"", "destination_id"=>"1pWmdUxPRO7Id3HkhlsDJfTViGLKzdfIVKZ_Ad0memkE", "settings"=>{"library"=>"KeheWeeklyMCB"}}, {"external_id"=>"hkoarkqejsvb", "name"=>nil, "destination_id"=>"1pWmdUxPRO7Id3HkhlsDJfTViGLKzdfIVKZ_Ad0memkE", "settings"=>{"library"=>"KeheLateDeliveryFee"}}, {"external_id"=>"bqwnqipeffxj", "name"=>nil, "destination_id"=>"1pWmdUxPRO7Id3HkhlsDJfTViGLKzdfIVKZ_Ad0memkE", "settings"=>{"library"=>"KehePassThroughPromotion"}}, {"external_id"=>"yajcqtqeuwhd", "name"=>nil, "destination_id"=>"1pWmdUxPRO7Id3HkhlsDJfTViGLKzdfIVKZ_Ad0memkE", "settings"=>{"library"=>"KehePromotion"}}, {"external_id"=>"hjczbplazgti", "name"=>nil, "destination_id"=>"1pWmdUxPRO7Id3HkhlsDJfTViGLKzdfIVKZ_Ad0memkE", "settings"=>{"library"=>"KeheSlotting"}}, {"external_id"=>"yvxntiktsqho", "name"=>nil, "destination_id"=>"1sEjNPub3yXyEMdHCzLMLaUmOxNBNLiv5-o_GVNgutlo", "settings"=>{"library"=>"KeheAdInvoice"}}]
+parsers = [
+  {"external_id"=>"1Z5by0rrvu1tVw5K2nXZRAJL3ct2TM7GS", "name"=>"UNFI East Weekly MCB", "destination_id"=>"1pWmdUxPRO7Id3HkhlsDJfTViGLKzdfIVKZ_Ad0memkE", "settings"=>{"source"=>"google_drive", "library"=>"UNFIEastWeeklyMCB"}},
+  {"external_id"=>"xvexmuksclhe", "name"=>nil, "destination_id"=>"1pWmdUxPRO7Id3HkhlsDJfTViGLKzdfIVKZ_Ad0memkE", "settings"=>{"library"=>"UNFIWestWeeklyMCB"}},
+  {"external_id"=>"eylfucfqzted", "name"=>nil, "destination_id"=>"1pWmdUxPRO7Id3HkhlsDJfTViGLKzdfIVKZ_Ad0memkE", "settings"=>{"library"=>"UNFIEastChargeback"}},
+  {"external_id"=>"enowqxdfgcqg", "name"=>nil, "destination_id"=>"1pWmdUxPRO7Id3HkhlsDJfTViGLKzdfIVKZ_Ad0memkE", "settings"=>{"library"=>"UNFIEastDeductionQuarterly"}},
+  {"external_id"=>"azwkpkgfxroi", "name"=>"UNFI East Reclamation", "destination_id"=>"1pWmdUxPRO7Id3HkhlsDJfTViGLKzdfIVKZ_Ad0memkE", "settings"=>{"library"=>"UNFIEastReclamation"}},
+  {"external_id"=>"unkxjvdpcdwg", "name"=>"", "destination_id"=>"1pWmdUxPRO7Id3HkhlsDJfTViGLKzdfIVKZ_Ad0memkE", "settings"=>{"library"=>"KeheWeeklyMCB"}},
+  {"external_id"=>"hkoarkqejsvb", "name"=>nil, "destination_id"=>"1pWmdUxPRO7Id3HkhlsDJfTViGLKzdfIVKZ_Ad0memkE", "settings"=>{"library"=>"KeheLateDeliveryFee"}},
+  {"external_id"=>"bqwnqipeffxj", "name"=>nil, "destination_id"=>"1pWmdUxPRO7Id3HkhlsDJfTViGLKzdfIVKZ_Ad0memkE", "settings"=>{"library"=>"KehePassThroughPromotion"}},
+  {"external_id"=>"yajcqtqeuwhd", "name"=>nil, "destination_id"=>"1pWmdUxPRO7Id3HkhlsDJfTViGLKzdfIVKZ_Ad0memkE", "settings"=>{"library"=>"KehePromotion"}},
+  {"external_id"=>"hjczbplazgti", "name"=>nil, "destination_id"=>"1pWmdUxPRO7Id3HkhlsDJfTViGLKzdfIVKZ_Ad0memkE", "settings"=>{"library"=>"KeheSlotting"}},
+  {"external_id"=>"yvxntiktsqho", "name"=>nil, "destination_id"=>"1sEjNPub3yXyEMdHCzLMLaUmOxNBNLiv5-o_GVNgutlo", "settings"=>{"library"=>"KeheAdInvoice"}},
+  {"external_id"=>"lwaiygkncvdo", "name"=>nil, "destination_id"=>"1sEjNPub3yXyEMdHCzLMLaUmOxNBNLiv5-o_GVNgutlo", "settings"=>{"library"=>"KeheChargebackInvoiceHomestoreBilling"}}]
+# CHANGE DESTINATION IDS before production!!
 
 parsers.each do |parser_attributes|
   admin.parsers.find_or_create_by(parser_attributes)
---
+++ lib/parsers/kehe_chargeback_invoice_homestore_billing.rb
@@ -0,0 +1,66 @@
+module Parsers
+  class KeheChargebackInvoiceHomestoreBilling
+    class << self
+      def parse_rows(document)
+        invoice_data(document).deep_merge(
+        'file_name' => document['file_name']
+        ).deep_merge(
+        'uploaded_at' => document['uploaded_at']
+        )
+      end
+
+      def invoice_data(document)
+        parsed_meta_data(document).deep_merge(parsed_invoice_date(document)
+        ).deep_merge(parsed_totals(document))
+      end
+
+      def get_raw_data(document, type)
+        document[type].map {|row| row.values }
+      end
+
+      def invoice_num(meta_data)
+        binding.pry
+        invoice_num_rows = meta_data.select{|row| row.match(/invoice.*#/i) }
+        invoice_num = invoice_num_rows.first.gsub(/invoice.*#/i,'').strip
+        invoice_num.empty? ? alt_invoice_num(meta_data, invoice_num_rows.last) : invoice_num
+      end
+
+      def alt_invoice_num(meta_data, invoice_row)
+        idx = meta_data.index(invoice_row) + 1
+        meta_data[idx].split(' ').last
+      end
+
+      def parsed_meta_data(document)
+        parsed = {}
+        meta_data = get_raw_data(document,'meta_data').map do |row|
+          row.join(' ')
+        end
+
+        parsed['invoice number'] = invoice_num(meta_data)
+
+        type_row = meta_data.select{|row| row.match(/type.*:?/i) }.first
+        parsed['Type'] = type_row.gsub(/type:?/i,'')
+        parsed
+      end
+
+      def parsed_invoice_date(document)
+        binding.pry
+        date = get_raw_data(document, 'invoice_dates').flatten[0]
+        {'invoice_date' => date}
+      end
+
+      def parsed_totals(document)
+        binding.pry
+        totals = get_raw_data(document, 'totals').flatten
+        invoice_total_row = totals.select{|row| row.match(/invoice.*total/i) }.first
+        chargeback_amount = invoice_total_row.match(/\$\d+\.?\d+/)[0].gsub('$','')
+
+        ep_fee_row = totals.select{|row| row.match(/ep.*fee/i) }.first
+        ep_fee = !!ep_fee_row ? ep_fee_row.match(/\$\d+\.?\d+/)[0].gsub('$','') : ''
+
+        {'chargeback_amount' => chargeback_amount,
+          'ep_fee' => ep_fee}
+      end
+    end
+  end
+end

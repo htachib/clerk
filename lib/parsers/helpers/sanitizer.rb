fd991d2d16c656a0e1d3e1cf01735c67bef00043
Hideko <hideko.tachibana@gmail.com>
2019-06-23 00:07:29 +0100

    add method to extract invoice numf rom file name to helpers

--- lib/parsers/helpers/sanitizer.rb
+++ lib/parsers/helpers/sanitizer.rb
@@ -4,7 +4,7 @@ module Parsers
       def str_to_dollars(str_amount)
         return nil if !str_amount
         dollar_cents_arr = str_amount.split(/(\.|\s)/).select{ |str| str.match(/\d/) }
-        
+
         if dollar_cents_arr && dollar_cents_arr.length == 2
           return (dollar_cents_arr.first.to_f) + (dollar_cents_arr.last.to_f / 100)
         else
@@ -14,6 +14,10 @@ module Parsers
 
       def get_amount_str(totals_arr)
         totals_arr ? totals_arr.match(/\$\d+(\.|\s)?\d+/)[0].gsub('$','') : nil
+      end
+      
+      def invoice_num_from_file_name(document)
+        document['file_name'].split(' ').first
       end
 
     end

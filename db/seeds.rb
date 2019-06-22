b4866288532f481eb915a94e473d5a308a75eadc
Hideko <hideko.tachibana@gmail.com>
2019-06-23 00:43:39 +0100

    working kehe reclamation parser

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,7 +23,8 @@ parsers = [
   {"external_id"=>"xzhgcymbtodl", "name"=>nil, "destination_id"=>"1sEjNPub3yXyEMdHCzLMLaUmOxNBNLiv5-o_GVNgutlo", "settings"=>{"library"=>"KeheLumperFeeChargebackInvoice"}},
   {"external_id"=>"uuopbrjehxkh", "name"=>nil, "destination_id"=>"1sEjNPub3yXyEMdHCzLMLaUmOxNBNLiv5-o_GVNgutlo", "settings"=>{"library"=>"KeheMerchandisingInvoice"}},
   {"external_id"=>"fcpastlnejqk", "name"=>nil, "destination_id"=>"1sEjNPub3yXyEMdHCzLMLaUmOxNBNLiv5-o_GVNgutlo", "settings"=>{"library"=>"KeheNewItemSetup"}},
-  {"external_id"=>"sjyigeaaixuy", "name"=>nil, "destination_id"=>"1sEjNPub3yXyEMdHCzLMLaUmOxNBNLiv5-o_GVNgutlo", "settings"=>{"library"=>"KeheNonserviceInStoreCredits"}}
+  {"external_id"=>"sjyigeaaixuy", "name"=>nil, "destination_id"=>"1sEjNPub3yXyEMdHCzLMLaUmOxNBNLiv5-o_GVNgutlo", "settings"=>{"library"=>"KeheNonserviceInStoreCredits"}},
+  {"external_id"=>"jmqonywollnj", "name"=>nil, "destination_id"=>"1sEjNPub3yXyEMdHCzLMLaUmOxNBNLiv5-o_GVNgutlo", "settings"=>{"library"=>"KeheReclamation"}}
 ]
 # CHANGE DESTINATION IDS before production!!
 
---
+++ lib/parsers/kehe_reclamation.rb
@@ -0,0 +1,71 @@
+module Parsers
+  class KeheReclamation < Base
+    class << self
+      def parse_rows(document)
+        invoice_data(document).deep_merge(
+        'file_name' => document['file_name']
+        ).deep_merge(
+        'uploaded_at' => document['uploaded_at']
+        )
+      end
+
+      def invoice_data(document)
+        parsed_meta_data(document).deep_merge(parsed_invoice_date(document)
+        ).deep_merge(parsed_totals(document)
+        ).deep_merge(parsed_customer(document))
+      end
+
+      def get_raw_data(document, type)
+        document[type].map { |row| row.values }
+      end
+
+      def invoice_num(meta_data)
+        invoice_num_rows = meta_data.select{|row| row.match(/invoice.*#/i) }
+        invoice_num = invoice_num_rows.first.gsub(/invoice.*#/i,'').strip
+        invoice_num.empty? ? alt_invoice_num(meta_data, invoice_num_rows.last) : invoice_num
+      end
+
+      def alt_invoice_num(meta_data, invoice_row)
+        idx = meta_data.index(invoice_row) + 1
+        meta_data[idx].split(' ').last
+      end
+
+      def sanitized_customer(rows)
+        regex = /(chargeback|invoice|reclamation|recovery|date.*invoice$)/i
+        rows.join('').gsub(regex,'').strip
+      end
+
+      def parsed_customer(document)
+        rows = get_raw_data(document, 'customer').flatten
+        customer = sanitized_customer(rows)
+        {'detailed_customer' => customer}
+      end
+
+      def parsed_meta_data(document)
+        parsed = {}
+        meta_data = get_raw_data(document,'meta_data').map do |row|
+          row.join(' ')
+        end
+
+        parsed['invoice number'] = invoice_num_from_file_name(document) || invoice_num(meta_data)
+        type_row = meta_data.select{|row| row.match(/type.*:?/i) }.first
+        parsed['Type'] = type_row.gsub(/type\W?/i,'')
+        parsed
+      end
+
+      def parsed_invoice_date(document)
+        date = get_raw_data(document, 'invoice_date').flatten[0]
+        {'invoice_date' => date}
+      end
+
+      def parsed_totals(document)
+        totals = get_raw_data(document, 'totals').flatten
+        invoice_total_row = totals.select{ |row| row.match(/$/)}.last
+        invoice_total = get_amount_str(invoice_total_row)
+        chargeback_amount = str_to_dollars(invoice_total)
+
+        {'chargeback_amount' => chargeback_amount}
+      end
+    end
+  end
+end

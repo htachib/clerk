2d7977531d11b70db42ccc137603d8952b6c3a21
Hideko <hideko.tachibana@gmail.com>
2019-06-27 16:12:46 +0100

    sanitize helper method get totals

--- lib/parsers/kehe_new_item_setup.rb
+++ lib/parsers/kehe_new_item_setup.rb
@@ -31,12 +31,16 @@ module Parsers
       end
 
       def parsed_totals(document)
-        totals = get_raw_data(document, 'totals').flatten
+        ep_fee_regex = /ep.*fee/i
+
+        totals = get_totals(document)
         invoice_total_row = totals ? totals.select{ |row| row.match(/$/)}.last : nil
         invoice_total = get_amount_str(invoice_total_row)
         chargeback_amount = str_to_dollars(invoice_total)
+        ep_fee_amount = get_total_in_dollars(totals, ep_fee_regex)
 
-        {'chargeback_amount' => chargeback_amount}
+        {'chargeback_amount' => chargeback_amount,
+          'ep_fee' => ep_fee_amount}
       end
     end
   end
--- lib/parsers/kehe_nonservice_in_store_credits.rb
+++ lib/parsers/kehe_nonservice_in_store_credits.rb
@@ -2,7 +2,7 @@ module Parsers
   class KeheNonserviceInStoreCredits < Base
     class << self
       include Parsers::Helpers::KeheSanitizers
-      
+
       def invoice_data(document)
         parsed_meta_data(document).deep_merge(parsed_invoice_date(document)
         ).deep_merge(parsed_totals(document)
@@ -47,12 +47,16 @@ module Parsers
       end
 
       def parsed_totals(document)
-        totals = get_raw_data(document, 'totals').flatten
+        ep_fee_regex = /ep.*fee/i
+
+        totals = get_totals(document)
         invoice_total_row = totals ? totals.select{ |row| row.match(/$/)}.last : nil
         invoice_total = get_amount_str(invoice_total_row)
         chargeback_amount = str_to_dollars(invoice_total)
+        ep_fee_amount = get_total_in_dollars(totals, ep_fee_regex)
 
-        {'chargeback_amount' => chargeback_amount}
+        {'chargeback_amount' => chargeback_amount,
+          'ep_fee' => ep_fee_amount}
       end
     end
   end

a1b0e5d135925eb1bd19901ad6ab74f71926256e
Hideko <hideko.tachibana@gmail.com>
2019-06-27 16:13:31 +0100

    cleanup sanitizer methods

--- lib/parsers/kehe_pass_through_promotion.rb
+++ lib/parsers/kehe_pass_through_promotion.rb
@@ -67,7 +67,6 @@ module Parsers
         invoice_total_row = totals ? totals.select{ |row| row.match(/$/)}.last : nil
         invoice_total = get_amount_str(invoice_total_row)
         chargeback_amount = str_to_dollars(invoice_total)
-
         ep_fee_amount = get_total_in_dollars(totals, ep_fee_regex)
 
         {'chargeback_amount' => chargeback_amount,
--- lib/parsers/kehe_retailer_store_placement.rb
+++ lib/parsers/kehe_retailer_store_placement.rb
@@ -2,11 +2,11 @@ module Parsers
   class KeheRetailerStorePlacement < Base
     class << self
       include Parsers::Helpers::KeheSanitizers
-      ## next optimization
-      def invoice_num(meta_data)
-        invoice_num_rows = meta_data.select{|row| row.match(/invoice.*#/i) }
-        invoice_num = invoice_num_rows.first.gsub(/invoice.*#/i,'').strip
-        invoice_num.empty? ? alt_invoice_num(meta_data, invoice_num_rows.last) : invoice_num
+
+      def parsed_invoice_number(meta_data)
+        row_regex = /invoice.*#/i
+        str_regex = /invoice.*#/i
+        sanitize_invoice_num(meta_data, row_regex, str_regex)
       end
 
       def alt_invoice_num(meta_data, invoice_row)
@@ -14,36 +14,38 @@ module Parsers
         meta_data[idx].split(' ').last
       end
 
-      def parsed_meta_data(document)
-        parsed = {}
-        meta_data = get_raw_data(document,'meta_data').map do |row|
-          row.join(' ')
-        end
+      def parsed_type(meta_data)
+        regex = /type.*:?/i
+        type_row = string_match_from_arr(meta_data, regex)
+        type_row.to_s.gsub(/type\W?/i,'').strip
+      end
 
-        parsed['invoice number'] = invoice_num_from_file_name(document) || invoice_num(meta_data)
-        type_row = meta_data.select{|row| row.match(/type.*:?/i) }.first
-        parsed['Type'] = type_row.gsub(/type\W?/i,'').strip
-        parsed
+      def parsed_meta_data(document)
+        meta_data = get_meta_data(document)
+        invoice_number = invoice_num_from_file_name(document) || parsed_invoice_number(meta_data)
+        type = parsed_type(meta_data)
+
+        {'invoice number' => invoice_number,
+          'Type' => type}
       end
 
       def parsed_invoice_date(document)
-        date = get_raw_data(document, 'invoice_date').flatten[0]
+        invoice_date_row = get_raw_data(document, 'invoice_date')
+        date = get_invoice_date(invoice_date_row, document)
         {'invoice_date' => date}
       end
 
       def parsed_totals(document)
-        totals = get_raw_data(document, 'totals').flatten
-        invoice_total_row = totals.last
+        ep_fee_regex = /ep.*fee/i
+
+        totals = get_totals(document)
+        invoice_total_row = totals ? totals.select{ |row| row.match(/$/)}.last : nil
         invoice_total = get_amount_str(invoice_total_row)
-        total_in_dollars = str_to_dollars(invoice_total)
+        chargeback_amount = str_to_dollars(invoice_total)
+        ep_fee_amount = get_total_in_dollars(totals, ep_fee_regex)
 
-
-        ep_fee = totals.select{|row| row.match(/ep.*fee/i) }.first
-        ep_fee_str = get_amount_str(ep_fee)
-        ep_fee_in_dollars = str_to_dollars(ep_fee_str)
-
-        {'chargeback_amount' => total_in_dollars,
-          'ep_fee' => ep_fee_in_dollars}
+        {'chargeback_amount' => chargeback_amount,
+          'ep_fee' => ep_fee_amount}
       end
     end
   end

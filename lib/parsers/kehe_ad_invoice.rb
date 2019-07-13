963a839be8246dd106344a8f84f18c05db17d7aa
Hideko <hideko.tachibana@gmail.com>
2019-07-13 14:13:48 +0100

    kehe ad invoice revisions

--- lib/parsers/kehe_ad_invoice.rb
+++ lib/parsers/kehe_ad_invoice.rb
@@ -3,6 +3,15 @@ module Parsers
     class << self
       include Parsers::Helpers::KeheSanitizers
 
+      def invoice_data(document)
+        parsed_meta_data(document).deep_merge(
+        parsed_invoice_date(document)).deep_merge(
+        parsed_totals(document)).deep_merge(
+        parsed_deduction_description(document)).deep_merge(
+        parsed_customer_chain(document)).deep_merge(
+        parsed_promo_date_range(document))
+      end
+
       def parsed_invoice_number(meta_data)
         row_regex = /invoice.*#/i
         str_regex = /invoice.*#/i
@@ -48,6 +57,46 @@ module Parsers
       def parsed_ep_fee(totals)
         ep_fee_regex = /ep.*fee/i
         get_total_in_dollars(totals, ep_fee_regex)
+      end
+
+      def parsed_deduction_description(document)
+        data = get_raw_data(document,'deduction_description').flatten
+        filtered = data.select { |row| row.length > 5 }
+        deduction_description = filtered.join(' ').gsub(/[^\s^\w^\/]/,'')
+        {'deduction_description' => deduction_description}
+      end
+
+      def parsed_customer_chain(document)
+        options = ['customer_name_option_1', 'customer_name_option_2', 'customer_name_option_3']
+        customer_chain_data = options.map { |option| get_raw_data(document, option) }.flatten
+        customer_chain = customer_chain_data.select { |c| c.length > 3 }.first.try(:titleize) || 'KeHE'
+        {'customer_chain' => customer_chain}
+      end
+
+      def parsed_promo_date_range(document)
+        options = ['date_range_option_1', 'date_range_option_2', 'date_range_option_3',
+                   'date_range_option_4']
+        promo_dates_data = options.map { |option| get_raw_data(document, option) }.flatten
+        promo_dates = promo_dates_data.select { |c| c.length > 3 }.first.strip
+        {'promo_dates' => get_promo_dates(promo_dates)}
+      end
+
+      def date_promo_month(year_int, month_int, date_int)
+        DateTime.new(year_int, month_int, date_int).strftime("%m/%d/%y")
+      end
+
+      def get_promo_dates(string)
+        if string.match?(/[a-z]{3,}/i)
+          month_string = string.match(/[a-z]+/i).try(:[], 0)
+          month_int = month_int_from_string(month_string)
+          year_int = string.scan(/[0-9]+/i).first.to_i
+          start_date = date_promo_month(year_int, month_int, 1)
+          end_date = date_promo_month(year_int, month_int, -1)
+        elsif string.scan(/\//i).count == 4
+          start_date, end_date = string.try(:scan, /\d{1,2}\/\d{1,2}\/\d{2,4}/)
+        end
+        {'start_date' => start_date,
+         'end_date' => end_date}
       end
     end
   end

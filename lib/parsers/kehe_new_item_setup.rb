993defc08b05234f65ed2b42b3a5b7aca00df2cf
Hideko <hideko.tachibana@gmail.com>
2019-07-13 21:14:31 +0100

    kehe new item setup parser revision

--- lib/parsers/kehe_new_item_setup.rb
+++ lib/parsers/kehe_new_item_setup.rb
@@ -3,6 +3,14 @@ module Parsers
     class << self
       include Parsers::Helpers::KeheSanitizers
 
+      def invoice_data(document)
+        parsed_meta_data(document).deep_merge(
+        parsed_invoice_date(document)).deep_merge(
+        parsed_totals(document)).deep_merge(
+        parsed_deduction_description(document)).deep_merge(
+        parsed_promo_date_range(document))
+      end
+
       def parsed_invoice_number(meta_data)
         row_regex = /invoice.*#/i
         str_regex = /invoice.*#/i
@@ -48,6 +56,17 @@ module Parsers
       def parsed_ep_fee(totals)
         ep_fee_regex = /ep.*fee/i
         get_total_in_dollars(totals, ep_fee_regex)
+      end
+
+      def parsed_deduction_description(document)
+        data = get_raw_data(document,'deduction_description').flatten.try(:first)
+        {'deduction_description' => data}
+      end
+
+      def parsed_promo_date_range(document)
+        data = get_raw_data(document,'promo_dates').flatten.try(:first)
+        {'start_date' => data,
+         'end_date' => data}
       end
     end
   end

130a94919ffaee8066a41544f71780ed3ba5844b
Hideko <hideko.tachibana@gmail.com>
2019-07-13 23:30:30 +0100

    kehe retailer store placement parser revisions

--- lib/mappers/kehe_retailer_store_placement.rb
+++ lib/mappers/kehe_retailer_store_placement.rb
@@ -9,12 +9,12 @@ module Mappers
         prepared_row['File Name'] = file_name
         prepared_row['Invoice Number'] = raw_rows['invoice number']
         prepared_row['Deduction Post Date'] = Date.parse(raw_rows['uploaded_at']).strftime("%m/%d/%Y")
-        prepared_row['Promo End Date'] = raw_rows['invoice_date']
-        prepared_row['Promo Start Date'] = raw_rows['start_date']
+        prepared_row['Promo End Date'] = raw_rows['promo_date']
+        prepared_row['Promo Start Date'] = raw_rows['promo_date']
         prepared_row['Deduction Type'] = raw_rows['Type']
-        prepared_row['Deduction Description'] = ''
-        prepared_row['Customer Chain ID'] = 'KeHE'
-        prepared_row['Customer Detailed Name'] = 'KeHE'
+        prepared_row['Deduction Description'] = raw_rows['deduction_description']
+        prepared_row['Customer Chain ID'] = raw_rows['customer_chain']
+        prepared_row['Customer Detailed Name'] = raw_rows['customer_chain']
         prepared_row['Chargeback Amount'] = raw_rows['chargeback_amount']
         prepared_row['EP Fee'] = raw_rows['ep_fee']
         prepared_row.values # => [['asdf', 'asdf', 'asdf']]

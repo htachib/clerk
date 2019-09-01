7c954fc209c458776482ab9bca870cb0ad7ec743
Hideko <hideko.tachibana@gmail.com>
2019-09-01 23:12:43 +0200

    extend limit to fetch from docparser

--- app/services/doc_parser_service.rb
+++ app/services/doc_parser_service.rb
@@ -2,8 +2,8 @@ class DocParserService
   API_KEY = ENV['DOC_PARSER_API_KEY']
   BASE_URL = 'https://api.docparser.com/v1'.freeze
 
-  def self.fetch_documents(parser_id)
+  def self.fetch_documents(parser_id, limit=200)
     url = BASE_URL + "/results/#{parser_id}"
-    HTTParty.get(url + "?api_key=#{API_KEY}").parsed_response
+    HTTParty.get(url + "?api_key=#{API_KEY}&limit=#{limit}").parsed_response
   end
 end

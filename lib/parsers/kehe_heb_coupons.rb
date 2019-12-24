34ce733b37f99414f45d4b3db76e5ff5dad77c12
Hideko <hideko.tachibana@gmail.com>
2019-12-23 22:01:28 -0500

    kehe heb coupons parser working

--- lib/parsers/kehe_heb_coupons.rb
+++ lib/parsers/kehe_heb_coupons.rb
@@ -2,47 +2,25 @@ module Parsers
   class KeheHebCoupons < Base
     class << self
       def invoice_data(document)
-        parsed_meta_data(document).deep_merge(
-        parsed_invoice_date(document)).deep_merge(
+        parsed_invoice_date(document).deep_merge(
         parsed_totals(document)).deep_merge(
-        parsed_deduction_description(document)).deep_merge(
-        parsed_promo_date_range(document))
+        parsed_invoice_number(document)).deep_merge(
+        parsed_shipped(document))
       end
 
-      def parsed_invoice_number(meta_data)
-        row_regex = /invoice.*#/i
-        str_regex = /invoice.*#/i
-        sanitize_invoice_num(meta_data, row_regex, str_regex)
-      end
-
-      def parsed_type(meta_data)
-        regex = /type.*:?/i
-        type_row = string_match_from_arr(meta_data, regex)
-        type_row.to_s.try(:gsub,/type:?/i,'').strip
-      end
-
-      def parsed_customer(meta_data)
-        regex = /master.*ref/i
-        customer_row = string_match_from_arr(meta_data, regex)
-        customer_row ? customer_row.to_s.try(:gsub,/coupon(.*)$/i,'').strip : nil
-      end
-
-      def parsed_meta_data(document)
-        meta_data = get_meta_data(document)
-        invoice_number = invoice_num_from_file_name(document) || parsed_invoice_number(meta_data)
-        type = parsed_type(meta_data)
-        customer = parsed_customer(meta_data)
-
-        {'invoice number' => invoice_number,
-          'Type' => type,
-          'customer' => customer,
-          'chain' => customer}
+      def parsed_invoice_number(document)
+        invoice_number = get_invoice_number(document)
+        {'invoice_number' => invoice_number}
       end
 
       def parsed_invoice_date(document)
-        invoice_date_row = get_raw_data(document, 'invoice_date')
-        date = get_invoice_date(invoice_date_row, document)
-        {'invoice_date' => date}
+        row = get_raw_data(document, 'invoice_date').try(:flatten) || []
+        invoice_date = row.try(:last)
+
+        {
+          'start_date' => invoice_date,
+          'end_date' => invoice_date
+        }
       end
 
       def parsed_totals(document)
@@ -55,25 +33,19 @@ module Parsers
       end
 
       def parsed_chargeback(totals)
-        invoice_total_row = totals ? totals.select{ |row| row.match(/$/)}.last : nil
-        invoice_total = get_amount_str(invoice_total_row)
-        str_to_dollars(invoice_total)
+        regex = /total.*invoice/i
+        get_total_in_dollars(totals, regex)
       end
 
       def parsed_ep_fee(totals)
-        ep_fee_regex = /ep.*fee/i
-        get_total_in_dollars(totals, ep_fee_regex)
+        regex = /ep.*fee/i
+        get_total_in_dollars(totals, regex)
       end
 
-      def parsed_promo_date_range(document)
-        data = get_raw_data(document,'po_date').flatten.try(:first)
-        {'start_date' => data,
-         'end_date' => data}
-      end
-
-      def parsed_deduction_description(document)
-        data = get_raw_data(document,'deduction_description').flatten.try(:first)
-        {'deduction_description' => data}
+      def parsed_shipped(document)
+        shipped_arr = document.try(:[], 'shipped')
+        shipped = shipped_arr.map { |h| h.try(:[], 'units').try(:to_i) }.try(:sum)
+        {'shipped' => shipped}
       end
     end
   end

da29edc490da9164fb466e9b05de606b566d5779
Hideko <hideko.tachibana@gmail.com>
2019-12-27 23:41:25 -0500

    local lookups

--- lib/mappers/helpers/base.rb
+++ lib/mappers/helpers/base.rb
@@ -6,17 +6,17 @@ module Mappers
       include GlobalLookups::ParserLookup
       include GlobalLookups::PlanningRetailerLookup
       include GlobalLookups::RetailChainNameLookup
-      include LocalLookups::GiantEagleReclamationCharges
-      include LocalLookups::KeheWeeklyMcb
-      include LocalLookups::NcgDeductionForm
-      include LocalLookups::UnfiEastDeductionInvoice
-      include LocalLookups::UnfiEastQualityDeduction
-      include LocalLookups::UnfiEastWeeklyMcb
-      include LocalLookups::UnfiEastWestOverpullSupplierBillingReport
-      include LocalLookups::UnfiEastWestVendorBillbackForm
-      include LocalLookups::UnfiEastWholeFoods
-      include LocalLookups::UnfiWestProductLossClaimsReport
-      include LocalLookups::UnfiWestWeeklyMcb
+      # include LocalLookups::GiantEagleReclamationCharges
+      # include LocalLookups::KeheWeeklyMcb
+      # include LocalLookups::NcgDeductionForm
+      # include LocalLookups::UnfiEastDeductionInvoice
+      # include LocalLookups::UnfiEastQualityDeduction
+      # include LocalLookups::UnfiEastWeeklyMcb
+      # include LocalLookups::UnfiEastWestOverpullSupplierBillingReport
+      # include LocalLookups::UnfiEastWestVendorBillbackForm
+      # include LocalLookups::UnfiEastWholeFoods
+      # include LocalLookups::UnfiWestProductLossClaimsReport
+      # include LocalLookups::UnfiWestWeeklyMcb
     end
   end
 end

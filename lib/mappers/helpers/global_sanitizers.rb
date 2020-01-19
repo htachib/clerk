ba316579d3cc457e96754a9cf72b85919295bfd4
Hideko <hideko.tachibana@gmail.com>
2020-01-19 19:25:39 -0300

    v2 update parsers

--- lib/mappers/helpers/global_sanitizers.rb
+++ lib/mappers/helpers/global_sanitizers.rb
@@ -3,11 +3,30 @@ module Mappers
     module GlobalSanitizers
 
       def set_variable_rate(prepared_row)
-        chargeback_amount = prepared_row['Chargeback Amount'] || 0
-        ep_fee = prepared_row['EP / Admin Fee'] || 0
-        shipped = prepared_row['Shipped'] || 0
+        chargeback_amount_str = prepared_row['Chargeback Amount'] || 0
+        ep_fee_str = prepared_row['EP / Admin Fee'] || 0
+        shipped_str = prepared_row['Shipped'] || 0
+
+        chargeback_amount = str_to_dollars(chargeback_amount_str)
+        ep_fee = str_to_dollars(ep_fee_str)
+        shipped = str_to_dollars(shipped_str)
 
         shipped == 0 || shipped == '' ? 0 : (chargeback_amount.to_f - ep_fee.to_f) / shipped.to_f
+      end
+
+      def str_to_dollars(str_amount)
+        return nil if !str_amount
+        amount = str_amount.to_s.try(:gsub,/(\,|\$)/,'')
+        dollar_cents_arr = amount.split(/(\.|\s)/).select{ |str| str.match?(/\d/) }
+        if dollar_cents_arr
+          if dollar_cents_arr.length == 2
+            return (dollar_cents_arr.first.to_f) + (dollar_cents_arr.last.to_f / 100)
+          elsif dollar_cents_arr.length == 1
+            return (dollar_cents_arr.first.to_f)
+          else
+            return ''
+          end
+        end
       end
     end
   end
---
+++ lib/mappers/kehe_safeway_scan_invoice.rb
@@ -0,0 +1,40 @@
+module Mappers
+  class KeheSafewayScanInvoice < Base
+    class << self
+      def prepare_rows(raw_rows)
+        raw_rows.map do |raw_row|
+          prepared_row = OutputHeaders::ROW_FIELDS.deep_dup
+          prepared_row['Customer'] = 'KeHE'
+          prepared_row['Parser'] = "KeHE Safeway Scan Invoice"
+          file_name = raw_row['file_name'].try(:gsub,'.pdf','').try(:gsub,'.PDF','')
+          prepared_row['File Name'] = file_name
+          prepared_row['Invoice Number'] = raw_row['invoice_number']
+          prepared_row['Deduction Post Date'] = Date.parse(raw_row['uploaded_at']).strftime("%m/%d/%Y")
+          prepared_row['Promo End Date'] = raw_row['end_date']
+          prepared_row['Promo Start Date'] = raw_row['start_date']
+          deduction_description = raw_row['deduction_description']
+          prepared_row['Deduction Description'] = deduction_description
+          prepared_row['Shipped'] = raw_row['shipped']
+          parser_lookup = get_parser_lookup(prepared_row['Parser'])
+          prepared_row['Deduction Type'] = parser_lookup['Deduction Type']
+          prepared_row['Customer Chain ID'] = parser_lookup['Customer Chain ID']
+          prepared_row['Customer Detailed Name'] = parser_lookup['Customer Detailed Name']
+          retail_chain_name_lookup = get_retail_chain_name_lookup(prepared_row['Customer Chain ID']) if prepared_row['Customer Chain ID']
+          prepared_row['Retail Chain Name'] = retail_chain_name_lookup['Retail Chain Name'] if retail_chain_name_lookup
+          planning_retailer_lookup = get_planning_retailer_lookup(prepared_row['Retail Chain Name']) if prepared_row['Retail Chain Name']
+          prepared_row['Planning Retailer'] = planning_retailer_lookup['Planning Retailer'] if planning_retailer_lookup
+          deduction_type_lookup = get_deduction_type_lookup(prepared_row['Deduction Type']) if prepared_row['Deduction Type']
+          prepared_row['Deduction Category'] = deduction_type_lookup['Deduction Category'] if deduction_type_lookup
+          prepared_row['Deduction Account'] = deduction_type_lookup['Deduction Account'] if deduction_type_lookup
+          prepared_row['Chargeback Amount'] = raw_row['chargeback_amount']
+          prepared_row['EP / Admin Fee'] = raw_row['ep_fee']
+          prepared_row['Variable Rate Per Unit'] = set_variable_rate(prepared_row)
+          prepared_row['Product'] = raw_row['upc']
+          prepared_row['Description'] = raw_row['product_information']
+          prepared_row['Customer Location'] = raw_row['customer_location']
+          prepared_row.values # => [['asdf', 'asdf', 'asdf']]
+        end
+      end
+    end
+  end
+end

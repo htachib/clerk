630ddf873eb7b04dd238d130686bac94f135cc6a
Hideko <hideko.tachibana@gmail.com>
2020-01-19 18:23:30 -0300

    v2 update parsers

---
+++ lib/parsers/kehe_roundys_scan_invoice.rb
@@ -0,0 +1,50 @@
+module Parsers
+  class KeheRoundysScanInvoice < Base
+    class << self
+      def invoice_data(document)
+        parsed_invoice_date(document).deep_merge(
+        parsed_totals(document)).deep_merge(
+        parsed_invoice_number(document)).deep_merge(
+        parsed_deduction_description(document))
+      end
+
+      def parsed_invoice_number(document)
+        invoice_number = get_invoice_number(document)
+        {'invoice_number' => invoice_number}
+      end
+
+      def parsed_invoice_date(document)
+        invoice_dates = document.try(:[], 'invoice_date').try(:first)
+
+        {
+          'start_date' => invoice_dates['start_date'],
+          'end_date' => invoice_dates['end_date']
+        }
+      end
+
+      def parsed_totals(document)
+        totals = get_totals(document)
+        chargeback_amount = parsed_chargeback(totals)
+        ep_fee_amount = parsed_ep_fee(totals)
+
+        {'chargeback_amount' => chargeback_amount,
+          'ep_fee' => ep_fee_amount}
+      end
+
+      def parsed_chargeback(totals)
+        regex = /invoice.*total/i
+        get_total_in_dollars(totals, regex)
+      end
+
+      def parsed_ep_fee(totals)
+        regex = /ep.*fee/i
+        get_total_in_dollars(totals, regex)
+      end
+
+      def parsed_deduction_description(document)
+        deduction_description = parsed_data(document, 'deduction_description')
+        { 'deduction_description' => deduction_description }
+      end
+    end
+  end
+end
---
+++ lib/parsers/kehe_safeway_promo_pass_through.rb
@@ -0,0 +1,97 @@
+module Parsers
+  class KeheSafewayPromoPassThrough < Base
+    class << self
+      def parse_rows(document)
+        rows = []
+        row_count = document.try(:[], 'upc').count
+
+        row_count.times do |row_idx|
+          meta_data = parsed_meta_data(document)
+          row_data = invoice_data(document, meta_data, row_idx)
+          rows.push(row_data)
+        end
+        total_row_data = total_row(document, rows[0])
+        rows.push(total_row_data)
+      end
+
+      def total_row(document, row)
+        ep_row = row.clone
+        ep_row['chargeback_amount'] = ep_fee(document)
+        ep_row['ep_fee'] = ep_fee(document)
+
+        deduction_description = ep_row['deduction_description']
+        deduction_description = 'EP FEE - ' + deduction_description
+        ep_row['deduction_description'] = deduction_description
+        ep_row['upc'] = nil
+        ep_row['product_information'] = nil
+        ep_row['customer_location'] = nil
+        ep_row
+      end
+
+      def parsed_meta_data(document)
+        parsed_invoice_number(document
+        ).deep_merge('uploaded_at' => document['uploaded_at']
+        ).deep_merge('file_name' => document['file_name'])
+      end
+
+      def invoice_data(document, meta_data, row_idx)
+        meta_data.deep_merge(
+        parsed_invoice_date(document, row_idx)).deep_merge(
+        parsed_totals(document, row_idx)).deep_merge(
+        parsed_deduction_description(document, row_idx)).deep_merge(
+        parsed_upc(document, row_idx)).deep_merge(
+        parsed_product_information(document, row_idx)).deep_merge(
+        parsed_customer_location(document, row_idx))
+      end
+
+      def parsed_invoice_number(document)
+        invoice_number = get_invoice_number(document)
+        {'invoice_number' => invoice_number}
+      end
+
+      def parsed_invoice_date(document, row_idx)
+        invoice_dates = document.try(:[], 'invoice_date').try(:[], row_idx)
+        {
+          'start_date' => invoice_dates['start_date'],
+          'end_date' => invoice_dates['end_date']
+        }
+      end
+
+      def parsed_totals(document, row_idx)
+        totals = get_totals(document)
+        chargeback_amount = totals.try(:[], row_idx)
+        { 'chargeback_amount' => chargeback_amount }
+      end
+
+      def parsed_deduction_description(document, row_idx)
+        parsed = document.try(:[], 'deduction_description').try(:[], row_idx)
+        deduction_description = parsed.try(:values).try(:first)
+        { 'deduction_description' => deduction_description }
+      end
+
+      def parsed_upc(document, row_idx)
+        parsed = document.try(:[], 'upc').try(:[], row_idx)
+        upc = parsed.try(:values).try(:first)
+        { 'upc' => upc }
+      end
+
+      def parsed_product_information(document, row_idx)
+        parsed = document.try(:[], 'product_information').try(:[], row_idx)
+        product_information = parsed.try(:values).try(:first)
+        { 'product_information' => product_information }
+      end
+
+      def parsed_customer_location(document, row_idx)
+        parsed = document.try(:[], 'customer_location').try(:[], row_idx)
+        customer_location = parsed.try(:values).try(:first)
+        { 'customer_location' => customer_location }
+      end
+
+      def ep_fee(document)
+        regex = /ep.*fee/i
+        totals = parsed_data(document, 'ep_fee')
+        get_total_in_dollars(totals, regex)
+      end
+    end
+  end
+end

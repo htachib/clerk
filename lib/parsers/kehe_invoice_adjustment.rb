e8e30eb1b295d53868391f34d9e71861b2f94271
Hideko <hideko.tachibana@gmail.com>
2020-01-21 16:03:59 -0300

    v2 update parsers

--- lib/parsers/kehe_invoice_adjustment.rb
+++ lib/parsers/kehe_invoice_adjustment.rb
@@ -1,28 +1,45 @@
 module Parsers
   class KeheInvoiceAdjustment < Base
     class << self
-      def parsed_invoice_number(meta_data)
-        row_regex = /invoice.*number/i
-        str_regex = /invoice.*number.*:/i
-        sanitize_invoice_num(meta_data, row_regex, str_regex)
+      def invoice_data(document)
+        parsed_invoice_date(document).deep_merge(
+        parsed_totals(document)).deep_merge(
+        parsed_invoice_number(document)).deep_merge(
+        parsed_deduction_description(document))
       end
 
-      def sanitize_invoice_num(meta_data, row_regex, str_regex)
-        invoice_rows = invoice_rows(meta_data, row_regex)
-        get_invoice_number(invoice_rows, str_regex)
+      def parsed_invoice_number(document)
+        invoice_number = get_invoice_number(document)
+        {'invoice_number' => invoice_number}
       end
 
-      def parsed_meta_data(document)
-        meta_data = get_meta_data(document)
-        invoice_number = invoice_num_from_file_name(document) || parsed_invoice_number(meta_data)
-        {'invoice number' => invoice_number}
+      def parsed_invoice_date(document)
+        invoice_date = get_invoice_date(document)
+
+        {
+          'start_date' => invoice_date,
+          'end_date' => invoice_date
+        }
       end
 
       def parsed_totals(document)
         totals = get_totals(document)
-        chargeback_amount = totals.try(:first)
+        chargeback_amount = parsed_chargeback(totals)
 
-        {'chargeback_amount' => chargeback_amount}
+        { 'chargeback_amount' => chargeback_amount }
+      end
+
+      def parsed_chargeback(totals)
+        regex = /net.*deduction/i
+        get_total_in_dollars(totals, regex)
+      end
+
+      def parsed_deduction_description(document)
+        deduction_string = parsed_data(document, 'deduction_description')
+        deduction_type = deduction_string.try(:match?, /product\s*variance/i) ? 'Shortage' : 'Off Invoice Allowance'
+        deduction_description = deduction_string.try(:match?, /product\s*variance/i) ? 'Shortage' : 'Marketing Allowance Adjustment'
+        { 'deduction_description' => deduction_description,
+          'deduction_type' => deduction_type }
       end
     end
   end

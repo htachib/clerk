805d75b6eece3b896baab26c38d60668dc06a81a
Hideko <hideko.tachibana@gmail.com>
2020-01-29 20:46:45 -0300

    v2 update parsers

--- lib/parsers/unfi_east_reclaim_vendor_chargeback_report.rb
+++ lib/parsers/unfi_east_reclaim_vendor_chargeback_report.rb
@@ -7,35 +7,62 @@ module Parsers
         parsed_invoice_number(document))
       end
 
+      def txt_file?(document)
+        content = document.try(:[], 'content')
+        !!content
+      end
+
+      def parsed_invoice_date(document)
+        invoice_date = txt_file?(document) ? txt_parsed_invoice_date(document) : pdf_parsed_invoice_date(document)
+        {
+          'start_date' => invoice_date,
+          'end_date' => invoice_date,
+        }
+      end
+
+      def parsed_totals(document)
+        chargeback_amount = txt_file?(document) ? txt_parsed_totals(document) : pdf_parsed_totals(document)
+        { 'chargeback_amount' => chargeback_amount }
+      end
+
+      def parsed_invoice_number(document)
+        invoice_number = txt_file?(document) ? txt_parsed_invoice_number(document) : pdf_parsed_invoice_number(document)
+        {'invoice_number' => invoice_number}
+      end
+
       def get_content_rows(document)
         content = document.try(:[], 'content')
         content.try(:split, "\n")
       end
 
-      def parsed_invoice_number(document)
+      def txt_parsed_invoice_number(document)
         rows = get_content_rows(document)
         match_row = rows.select { |r| r.try(:match?, /total\s*reclaim\s*credit/i) }.try(:first)
-        invoice_number = match_row.try(:gsub, /^\s*/, '').try(:gsub, /total\s*reclaim\s*credit.*/i, '').try(:gsub, /\s*$/, '')
-        {'invoice_number' => invoice_number}
+        match_row.try(:gsub, /^\s*/, '').try(:gsub, /total\s*reclaim\s*credit.*/i, '').try(:gsub, /\s*$/, '')
       end
 
-      def parsed_invoice_date(document)
+      def txt_parsed_invoice_date(document)
         rows = get_content_rows(document)
         match_row = rows.select { |r| r.try(:match?, /period\s*ending/i) }.try(:first)
-        invoice_date = match_row.try(:scan, /\d{1,4}\/\d{1,4}\/\d{1,4}/).try(:first)
-
-        {
-          'start_date' => invoice_date,
-          'end_date' => invoice_date
-        }
+        match_row.try(:scan, /\d{1,4}\/\d{1,4}\/\d{1,4}/).try(:first)
       end
 
-      def parsed_totals(document)
+      def txt_parsed_totals(document)
         rows = get_content_rows(document)
         match_row = rows.select { |r| r.try(:match?, /vendor\s*total/i) }.try(:first)
-        chargeback_amount = match_row.try(:scan, /\d*\,?\d*\,?\d*\,?\d+\.?\d*/).try(:first)
+        match_row.try(:scan, /\d*\,?\d*\,?\d*\,?\d+\.?\d*/).try(:first)
+      end
 
-        { 'chargeback_amount' => chargeback_amount }
+      def pdf_parsed_invoice_number(document)
+        get_invoice_number(document)
+      end
+
+      def pdf_parsed_invoice_date(document)
+        get_invoice_date(document)
+      end
+
+      def pdf_parsed_totals(document)
+        get_totals(document).try(:first)
       end
     end
   end

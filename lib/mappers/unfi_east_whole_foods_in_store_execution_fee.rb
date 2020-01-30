9857421dfd62e19911ecc9a0f721dde72fd863ef
Hideko <hideko.tachibana@gmail.com>
2020-01-30 00:40:32 -0300

    match parser name without customer

--- lib/mappers/unfi_east_whole_foods_in_store_execution_fee.rb
+++ lib/mappers/unfi_east_whole_foods_in_store_execution_fee.rb
@@ -4,7 +4,7 @@ module Mappers
       def prepare_rows(raw_rows)
         prepared_row = OutputHeaders::ROW_FIELDS.deep_dup
         prepared_row['Customer'] = 'UNFI East'
-        prepared_row['Parser'] = "UNFI East Whole Foods In Store Execution Fee"
+        prepared_row['Parser'] = "Whole Foods In Store Execution Fee"
         file_name = raw_rows['file_name'].try(:gsub,'.pdf','').try(:gsub,'.PDF','')
         prepared_row['File Name'] = file_name
         prepared_row['Invoice Number'] = raw_rows['invoice_number']

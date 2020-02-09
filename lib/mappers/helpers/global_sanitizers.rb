4ca4a0866ea58005131db818835faf299cd0c9b4
Hideko <hideko.tachibana@gmail.com>
2020-02-09 14:59:21 -0300

    handle >2 digit cents string

--- lib/mappers/helpers/global_sanitizers.rb
+++ lib/mappers/helpers/global_sanitizers.rb
@@ -20,6 +20,7 @@ module Mappers
         dollar_cent_split = amount.try(:split, /(\.|\s)/)
         cents_str = dollar_cent_split.try(:length) == 3 ? dollar_cent_split.try(:last) : 0
         cents_str += '0' if cents_str.try(:gsub, /\W*/i, '').try(:length) == 1
+        cents_str = cents_str.try(:scan, /^\d{2}/).try(:first)
         dollar_str = dollar_cent_split.try(:first).try(:gsub, /\W*/i, '') || 0
         return '' if !cents_str && !dollar_str
         (dollar_str.to_f) + (cents_str.to_f / 100)

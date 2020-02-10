aa298b77dcbd25c70a50423bfdea1ebc16afb4ad
Hideko <hideko.tachibana@gmail.com>
2020-02-09 22:07:40 -0300

    remove local parser lookup strategy

--- lib/mappers/helpers/base.rb
+++ lib/mappers/helpers/base.rb
@@ -7,17 +7,6 @@ module Mappers
       include GlobalLookups::ParserLookup
       include GlobalLookups::PlanningRetailerLookup
       include GlobalLookups::RetailChainNameLookup
-      # include LocalLookups::GiantEagleReclamationCharges
-      # include LocalLookups::KeheWeeklyMcb
-      # include LocalLookups::NcgDeductionForm
-      # include LocalLookups::UnfiEastDeductionInvoice
-      # include LocalLookups::UnfiEastQualityDeduction
-      # include LocalLookups::UnfiEastWeeklyMcb
-      # include LocalLookups::UnfiEastWestOverpullSupplierBillingReport
-      # include LocalLookups::UnfiEastWestVendorBillbackForm
-      # include LocalLookups::UnfiEastWholeFoods
-      # include LocalLookups::UnfiWestProductLossClaimsReport
-      # include LocalLookups::UnfiWestWeeklyMcb
     end
   end
 end
